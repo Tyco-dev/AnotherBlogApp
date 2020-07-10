146cff087f3ddd6bf5078145009cae131b0e2179
Tyco <tillintj@gmail.com>
2020-07-10 06:42:32 -0400

    forms and user accounts

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Local
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
